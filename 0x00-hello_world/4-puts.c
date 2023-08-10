c9679b2600f2b11e5188fde70bd1d771e85fc302
Shiemyiano <aelshiemy21@gmail.com>
2023-08-10 16:37:40 -0700

    0x04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+
 /**
-* function to print
-*/
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
