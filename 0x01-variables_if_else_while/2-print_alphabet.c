152c72d5cb6e6463efe29e00d382c69bde34ba98
Shiemyiano <aelshiemy21@gmail.com>
2023-08-11 17:02:17 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *print all letter in small case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a= 'a';
+do
+{
+putchar(a);
+a++;
+}while(a <= 'z');
+return (0);
+}
