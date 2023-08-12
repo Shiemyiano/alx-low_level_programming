49077ea1d0aaf090e5e7bf91b09c362fbb79b803
Shiemyiano <aelshiemy21@gmail.com>
2023-08-11 19:20:37 -0700

    b3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,25 +14,29 @@ for (m = 0; m <= 9; m++)
 {
 for (k = 0; k <= 9; k++)
 {
-if ( m == 9 && k == 9)
+if (m == 9 && k == 9)
 break;
 for (j = 0; j <= 9; j++)
 {
 for (i = 0; i <= 9; i++)
 {
-if (i != j)
-{
+
 putchar('0' + m);
 putchar('0' + k);
 putchar(' ');
 putchar('0' + j);
 putchar('0' + i);
+if (m == 9 && k == 8 && i == 9 && j == 9)
+break;
 putchar(',');
 putchar(' ');
 }
 }
 }
+if (m == 9 && k == 8)
+break;
 }
+
 putchar('\n');
 return (0);
 }
