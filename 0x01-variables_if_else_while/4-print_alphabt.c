98375760a53488d3c423d3f2cec44c985f7985f1
Shiemyiano <aelshiemy21@gmail.com>
2023-08-11 17:28:26 -0700

    eq

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *print all letter in small case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a = 'a';
+for(;a <= 'z' ; a++)
+{
+if(a == 'e' || a == 'q')
+{
+continue;
+}
+putchar(a);
+}
+printf("\n");
+return (0);
+}
