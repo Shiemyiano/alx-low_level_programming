4ba5256c9c42423952be4cc6888596b8615e0dc6
Shiemyiano <aelshiemy21@gmail.com>
2023-08-15 16:46:35 -0700

    lst

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,12 +10,16 @@
 
 void jack_bauer(void)
 {
-int h1,h2;
-char m1,m2;
+int h1, h2;
+char m1, m2;
 for (h1 = 0; h1 < 3; h1++)
 {
-for (h2 = 0; h2 < 4; h2++)
+for (h2 = 0; h2 < 9; h2++)
 {
+if (h1 == 2 && h2 == 4)
+{
+break;
+}
 for (m1 = 0; m1 < 6; m1++)
 {
 for (m2 = 0; m2 < 10; m2++)
