3573b203955fa16c22d44abeb1e247a053de21c3
Shiemyiano <aelshiemy21@gmail.com>
2023-08-15 16:39:18 -0700

    hello

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - wirte all minutes
+ * void: no argument required
+ *
+ * no return
+ *
+ **/
+
+void jack_bauer(void)
+{
+int h1,h2;
+char m1,m2;
+for (h1 = 0; h1 < 3; h1++)
+{
+for (h2 = 0; h2 < 4; h2++)
+{
+for (m1 = 0; m1 < 6; j++)
+{
+for (m2 = 0; m2 < 10; i++)
+{
+_putchar(h1);
+_putchar(h2);
+_putchar(':');
+_putchar(m1);
+_putchar(m2);
+}
+_putchar('\n');
+}
+}
