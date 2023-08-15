83de774cc859c0184c3ab86f4b9702bd2a6bfd25
Shiemyiano <aelshiemy21@gmail.com>
2023-08-15 15:18:36 -0700

    0x01

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
+/**
+ * print_alphabet - wirte all alphabet
+ * void: no argument required
+ *
+ * no return
+ *
+ **/
+
 void print_alphabet(void)
 {
 char i;
-for (i ='a';i <= 'z';i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
+}
 _putchar('\n');
-} 
 }
