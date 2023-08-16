2c3d65439003d6582e93c649cc1b3ef48e148760
Shiemyiano <aelshiemy21@gmail.com>
2023-08-15 17:17:26 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * int add(int, int) - funcation that sum two numbers
+ * @num1: first argument
+ * @num2: second argument
+ *
+ * Return: retuen summation of two numbers
+ *
+ **/
+
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
+
