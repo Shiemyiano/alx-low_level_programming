846c8d8a81421cdfd21ae5bb72539ae27912d000
Shiemyiano <aelshiemy21@gmail.com>
2023-08-15 17:30:45 -0700

    counter

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - function to print between given number to 98 
+ * @n: start number to count from 
+ * Return: no return 
+ */
+void print_to_98(int n)
+{
+if (n > 98)
+{
+for (; n >= 98 ; n--)
+{
+printf("%d \,", n);
+}
+}
+else
+{
+for (; n <= 98 ; n++)
+{
+printf("%d \,", n);
+}
+}
+}
