7f366b76e0b45c3b82890fa71d8e1a82905c9e85
Shiemyiano <aelshiemy21@gmail.com>
2023-08-15 17:02:28 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,18 +11,19 @@
 void times_table(void)
 {
 int m1, m2;
-int result;
+int result, r;
 for (m1 = 0; m1 < 10; m1++)
 {
 for (m2 = 0; m2 < 10; m2++)
 {
 result = m1 * m2;
-_putchar(result + '0');
-if (m2 == 9)
+if (result > 9)
 {
-_putchar('$');
-break;
+r = result / 10;
+result = result % 10;
+_putchar(r + '0');
 }
+_putchar(result + '0');
 _putchar(',');
 _putchar(' ');
 }
