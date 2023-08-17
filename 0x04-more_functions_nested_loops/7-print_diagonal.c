c497f6df0417cd66bd05fc43b3d7844792b814bc
Shiemyiano <aelshiemy21@gmail.com>
2023-08-17 14:48:27 -0700

    hanet

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - function that checks for uppercase character.
+ * @n: is the integer for the paramaters of my function
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+int i;
+int j;
+for (i = 1; i <= n; i++)
+{
+_putchar('\\');
+_putchar('\n');
+for (j = 1; j <= i; j++)
+{
+_putchar(' ');
+}
+}
+if (n <= 0)
+_putchar('\n');
+}
