957662242eaa2f67396a3945c6382a77d5362175
Shiemyiano <aelshiemy21@gmail.com>
2023-08-17 12:40:19 -0700

    upper0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+/*
+ * 
+ * _isupper(int) - function that check if arguments is char
+ * @c argument to  check
+ * Return: 1 is uppercase
+ * otherwise return 0
+ *
+ * */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+ _isupper(char)
+#include <stdio.h>
