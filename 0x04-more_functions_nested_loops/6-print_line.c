a4aae7679072f5048e60790dd930c1b98850b44b
Shiemyiano <aelshiemy21@gmail.com>
2023-08-17 14:29:37 -0700

    a

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,11 @@
  */
 void print_line(int n)
 {
-int a = 0;
+int i;
 
-if (n > 0)
-{
-while (a <= n)
+for (i =0; i <= n; i++)
 {
 _putchar('_');
-a++;
 }
-_putchar('\n');
-}
-else
 _putchar('\n');
 }
