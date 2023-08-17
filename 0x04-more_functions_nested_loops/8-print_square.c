45f055e937c31c80ce45cbb74a9bcdf6af1c76b1
Shiemyiano <aelshiemy21@gmail.com>
2023-08-17 15:09:31 -0700

    squar

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - function that draw of hash.
+ * @size: the length and width of square
+ * Return: 0
+ */
+void print_square(int size)
+{
+int i;
+int j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+if (n <= 0)
+_putchar('\n');
+}
