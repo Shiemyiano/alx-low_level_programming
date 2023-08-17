ad983739ab85bb2213cebc50f6ced67c38b32ae1
Shiemyiano <aelshiemy21@gmail.com>
2023-08-17 13:14:58 -0700

    m

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that check if arguments digit
+ * @a: argument to check
+ * @b: second args
+ * Return: mul a*b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,4 +7,6 @@ int _isupper(int);
 
 int _isdigit(int);
 
+int mul(int a, int b);
+
 #endif /* MAIN_H */
