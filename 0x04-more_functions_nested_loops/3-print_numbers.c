525550834d3609e6d81756531145659f4e714e5a
Shiemyiano <aelshiemy21@gmail.com>
2023-08-17 13:19:51 -0700

    m

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print numbers
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+print("\n");
+}
