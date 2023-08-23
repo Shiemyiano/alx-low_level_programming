d8a2c64ce92cd450726aa221faca742b50e68c5e
Shiemyiano <aelshiemy21@gmail.com>
2023-08-23 15:20:02 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,20 @@
 /**
- * _strcat - fuction to concatenate two strings
- * @dest: first string
- * @src: second string
- *
- * Return: poiter to concatenate string
+ * _strncat - concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting `dest`
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-int i;
-int c;
+int i, c;
 
 for (i = 0; dest[i] != '\0'; i++);
 
-for (c = 0; src[c] != '\0'; c++)
+for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
 {
 dest[i] = src[c];
-i++;
 }
-
-dest[i] = '\0';
-
 return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,20 @@
 /**
- * _strncat - fuction to concatenate two strings
- * @dest: first string
- * @src: second string
- * @n: length to cut
- * Return: poiter to concatenate string
+ * _strncat - concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting `dest`
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-int c;
-
+int i, c;
+	
 for (i = 0; dest[i] != '\0'; i++);
 
-for (c = 0; src[c] != '\0'; c++)
+for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
 {
-if (c == n )
-{
-break;
-}
 dest[i] = src[c];
-i++;
 }
-
-dest[i] = '\0';
-
 return (dest);
 }
