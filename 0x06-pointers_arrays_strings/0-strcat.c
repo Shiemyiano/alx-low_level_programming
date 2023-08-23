d676d1a76716df507daba1a6bfc4e5110979410a
Shiemyiano <aelshiemy21@gmail.com>
2023-08-23 15:27:41 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 /**
- * _strncat - concatenate two strings
- * @dest: string
- * @src: string
- * Return: pointer to resulting `dest`
+ * _strcat - concatenate two strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * Return: pointer to resulting string `dest`
  */
 
 char *_strcat(char *dest, char *src)
@@ -11,10 +11,12 @@ int i, c;
 
 for (i = 0; dest[i] != '\0'; i++);
 
-for (c = 0; src[c] != '\0'; c++, i++)
+for (c = 0; src[c] != '\0'; c++)
 {
 dest[i] = src[c];
+i++;
 }
-dest[++i] = '\0';
+
+dest[i] = '\0';
 return (dest);
 }
