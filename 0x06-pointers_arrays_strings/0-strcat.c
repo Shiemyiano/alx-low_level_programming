ebd93927e31d77628251d8508c51d0b291199eef
Shiemyiano <aelshiemy21@gmail.com>
2023-08-23 15:22:35 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,19 +2,19 @@
  * _strncat - concatenate two strings
  * @dest: string
  * @src: string
- * @n: number of elements to concatenate in
  * Return: pointer to resulting `dest`
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 int i, c;
 
 for (i = 0; dest[i] != '\0'; i++);
 
-for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
+for (c = 0; src[c] != '\0'; c++, i++)
 {
 dest[i] = src[c];
 }
+dest[++i] = '\0';
 return (dest);
 }
