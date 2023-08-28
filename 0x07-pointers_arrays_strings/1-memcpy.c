6d7daf9a227464da71eb5d4d72335dd37a1f0b27
Shiemyiano <aelshiemy21@gmail.com>
2023-08-28 13:13:20 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+/** 
+ * _memcpy - function that copy memory as required
+ * @dest: name of array
+ * @src: filler 
+ * @n: number of location to fill
+ *
+ * Return: address of first elment of array
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+
+return (dest);
+}
