61d2d270d243e4800bd02cc0a2ea1f6627233785
Shiemyiano <aelshiemy21@gmail.com>
2023-08-28 14:18:43 -0700

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+/**
+ * _strstr- locates a character in a string
+ *
+ * @haystack: the string to check
+ * @needle: the character we're looking for
+ *
+ * Return: a pointer to the first occurance of the character @c in the string
+ * @s. Return NULL if the character isn't found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+int i, count, flag, j;
+i = 0;
+count = 0;
+flag =0;
+j = 0;
+
+while (needle[i] != '\0')
+count++;
+
+for (i = 0; haystack[i] != '\0'; i++)
+{
+if (haystack[i] = needle[j])
+{
+flag++;
+j++;
+if (flag == count)
+{
+return (haystack + i - flag);
+}
+}
+else
+{
+j = 0;
+flag = 0;
+}
+}
+
+}
