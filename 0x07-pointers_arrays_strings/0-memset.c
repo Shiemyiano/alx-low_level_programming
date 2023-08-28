03f74d18d1a728744a2c32c3fcc17ba66c1be340
Shiemyiano <aelshiemy21@gmail.com>
2023-08-28 13:00:03 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+/**
+ *
+ *
+ * _memset - function that fills memory as required
+ * @s: name of array
+ * b: filler 
+ * n: number of location to fill
+ *
+ * Return: address of first elment of array
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+
+return (s);
+}
