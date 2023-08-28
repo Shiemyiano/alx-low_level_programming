3cec7e71b74dc97f7764a310897e45509fecc025
Shiemyiano <aelshiemy21@gmail.com>
2023-08-28 13:24:51 -0700

    a

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - fuction locate char in string
+ * @s: string
+ * @c: char to find
+ *
+ * Return: pointer to required char
+ */
+
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (c == s[i]
+return (s + i);
+}
+return (NULL);
+}
