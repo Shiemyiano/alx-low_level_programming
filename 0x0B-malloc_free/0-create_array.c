c01c0b6ffca21fb5f2f966b057b6f096c747dbef
Shiemyiano <aelshiemy21@gmail.com>
2023-09-04 04:08:01 -0700

    a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+/*
+ * create_array - function name
+ * @size:  index of array 
+ * @c: init element
+ *
+ * Return: Null if size = 0 either address of first element
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *arr;
+if (size == 0)
+return ((void *) 0);
+else 
+{
+arr = malloc(size * sizeof(char));
+arr[0] = c;
+return (arr);
+}
+}
