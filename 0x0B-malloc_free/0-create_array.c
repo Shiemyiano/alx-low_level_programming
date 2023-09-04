4d2f6854c08a8f29d632066db5dbb2bdf97dae23
Shiemyiano <aelshiemy21@gmail.com>
2023-09-04 04:15:02 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,21 +1,22 @@
 #include <stdlib.h>
-/*
- * create_array - function name
- * @size:  index of array 
+/**
+ * create_array - function name, crearte dynamic array
+ * @size:  index of array
  * @c: init element
  *
  * Return: Null if size = 0 either address of first element
  *
  */
-
 char *create_array(unsigned int size, char c)
 {
+int i = 0;
 char *arr;
 if (size == 0)
 return ((void *) 0);
-else 
+else
 {
 arr = malloc(size * sizeof(char));
+for (; i < size; i++)
 arr[0] = c;
 return (arr);
 }
